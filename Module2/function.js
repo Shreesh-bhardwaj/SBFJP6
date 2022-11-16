3e9b53d3647b31508ccb42405320ebd74964de8a
shreesh-bhardwaj <shreeshbhardwaj9@gmail.com>
2022-11-16 12:47:41 +0530

    intro to JS

---
+++ Module2/function.js
@@ -0,0 +1,10 @@
+function sayhello(){
+    console.log("Hello JS WORLD");
+}
+sayhello();
+
+let a = function sum(num1, num2)
+{
+    return num1+num2;
+}
+console.log(a(2,3));
---
+++ Module2/intro.js
@@ -0,0 +1,9 @@
+console.log("Hello JS");
+
+let string ='this is shreesh JS';
+console.log(string);
+
+for(let i=1;i<5;i++)
+{
+    console.log(i);
+}
