742c49774a983a12fc7ab423a3328f41ff3c2020
shreesh-bhardwaj <shreeshbhardwaj9@gmail.com>
2022-11-17 16:38:03 +0530

    objects added

--- Module2/function.js
+++ Module2/function.js
@@ -8,3 +8,7 @@ let a = function sum(num1, num2)
     return num1+num2;
 }
 console.log(a(2,3));
+
+(function(n1,n2){
+    console.log(n1*n2);
+})(3,7);
---
+++ Module2/object.js
@@ -0,0 +1,24 @@
+// let obj ={
+//     Name: "Shreesh" ,
+//     Role: "QA Analyst",
+//     Age:26,
+//     "Phone Number": 8766342682
+// };
+// console.log(obj);
+
+let ShreeshDetails={
+    Name:"Shreesh",
+    Age: 26,
+    Friends: ['Hem','Manik','Rishabh'],
+    Address: {
+        City:'Bikaner',
+        PinCode: 334002
+    },
+    Role: 'QA',
+    Sayshi: function()
+    {
+        console.log("Sayshi");
+    },
+};
+
+console.log(ShreeshDetails);
